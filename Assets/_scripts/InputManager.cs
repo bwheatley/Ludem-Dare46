95adcac6b2f92131d99a0005c39daf175b408bc1
bill <bill@williamwheatley.com>
2020-04-19 17:15:06 -0400

    Add touch support

--- Assets/_scripts/InputManager.cs
+++ Assets/_scripts/InputManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class InputManager : MonoBehaviour {
 
     public LayerMask SharkBody;
+    public Vector2 startPos;
+    public Vector2 direction;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +19,45 @@ public class InputManager : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+
+
         //Left click!
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0) ) {
             Vector2 screenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             ObjectCheck(screenPos);
         }
+        // Track a single touch as a direction control.
+        else if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Handle finger movements based on TouchPhase
+            switch (touch.phase)
+            {
+                //When a touch has first been detected, change the message and record the starting position
+                case TouchPhase.Began:
+                    // Record initial touch position.
+                    startPos = touch.position;
+                    Debug.Log(string.Format("Begun "));
+                    Vector2 screenPos = Camera.main.ScreenToWorldPoint(touch.position);
+                    ObjectCheck(screenPos);
+
+                    break;
+
+                //Determine if the touch is a moving touch
+                case TouchPhase.Moved:
+                    // Determine direction by comparing the current touch position with the initial one
+                    direction = touch.position - startPos;
+                    Debug.Log(string.Format( "Moving "));
+                    break;
+
+                case TouchPhase.Ended:
+                    // Report that the touch has ended when it ends
+                    Debug.Log("Ending");
+                    break;
+            }
+        }
+
     }
 
 
